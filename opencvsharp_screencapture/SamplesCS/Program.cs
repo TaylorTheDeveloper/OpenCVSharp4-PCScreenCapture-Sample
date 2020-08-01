059208360f022a8f4c9f8c7e103cc6caa5ea669d
taylor brockhoeft <taylorbrockhoeft@outlook.com>
2020-08-01 15:38:00 -0700

    add screen capture code

---
+++ opencvsharp_screencapture/SamplesCS/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SamplesCS
+{
+    using System;
+
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ScreenCaptureSample sample = new ScreenCaptureSample();
+            sample.Run();
+        }
+    }
+}
